b51fb6e4774884ed11cf6f983afd5204fa914265
Olof Englund <olofenglund96@gmail.com>
2020-05-24 22:40:41 +0200

    Project 3 kinda finished

--- project3/main.R
+++ project3/main.R
@@ -77,15 +77,25 @@ for (i in 1:length(chosen)) {
   chosen.data[chosen.data$region == chosen[i], 'day_fall'] <- data$day_nbr_region*chosen.data[chosen.data$region == chosen[i], 'day_after'] + day_top[i,]*(1 - chosen.data[chosen.data$region == chosen[i], 'day_after'])
 }
 
-model.piece <- lm(log(new_cases + 1) ~ (day_rise + day_fall) * region, data = chosen.data)
-pred.piece <- cbind(chosen.data,
-                   fit = exp(predict(model.piece, chosen.data)))
+model.piece.linear <- lm(new_cases ~ (day_rise + day_fall) * region, data = chosen.data)
+model.piece.log <- lm(log(new_cases + 1) ~ (day_rise + day_fall) * region, data = chosen.data)
+model.piece.poly <- lm(new_cases ~ (I(day_rise^2) + I(day_fall^2)) * region, data = chosen.data)
 
-ggplot(pred.piece, aes(x = day_nbr_region, y = new_cases)) +
+
+pred.piece.linear <- cbind(chosen.data,
+                   fit = predict(model.piece.linear, chosen.data))
+pred.piece.log <- cbind(chosen.data,
+                           fit = exp(predict(model.piece.log, chosen.data)))
+pred.piece.poly <- cbind(chosen.data,
+                        fit = predict(model.piece.poly, chosen.data))
+
+
+ggplot(pred.piece.poly, aes(x = day_nbr_region, y = new_cases)) +
   geom_point() +
-  geom_line(data = pred.piece, aes(x = day_rise, y = fit), color = 'red') + 
-  geom_line(data = pred.piece, aes(x = day_fall, y = fit), color = 'blue') +
-  facet_wrap ( ~region )
+  geom_smooth(color='green') +
+  geom_line(data = pred.piece.poly, aes(x = day_rise, y = fit), color = 'red') + 
+  geom_line(data = pred.piece.poly, aes(x = day_fall, y = fit), color = 'blue') +
+  facet_wrap ( ~region, scales = "free_y" )
 
 X <- data.frame(day_nbr_region = c(), day_rise = c(), day_fall = c(), region = c())
 
@@ -111,21 +121,53 @@ ggplot(chosen.data[chosen.data$region == reg,], aes(x = day_nbr_region, y = new_
   geom_line(data = pred.piece.longer[pred.piece.longer$region == reg,], aes(y = conf.upr),
             color = "red", linetype = "dashed", size = 1)
 
-pred.piece$v <- influence(model.piece)$hat
+pred.piece.linear$v <- influence(model.piece.linear)$hat
+pred.piece.log$v <- influence(model.piece.log)$hat
+pred.piece.poly$v <- influence(model.piece.poly)$hat
+
+pred.piece.linear$e <- model.piece.linear$residuals
+pred.piece.log$e <- model.piece.log$residuals
+pred.piece.poly$e <- model.piece.poly$residuals
+
+pred.piece.linear$r <- rstudent(model.piece.linear)
+pred.piece.log$r <- rstudent(model.piece.log)
+pred.piece.poly$r <- rstudent(model.piece.poly)
 
 for (region in unique(pred.piece$region)) {
   pred.piece[(pred.piece$region == region), c("ubound")] <- with(pred.piece, 1/sum(pred.piece$region == region))
   pred.piece[(pred.piece$region == region), c("lbound")] <- with(pred.piece, 2*3/sum(pred.piece$region == region))
 }
 
-ggplot(data = pred.piece, aes(x = day_nbr_region, y = v, color = region)) +
-  geom_jitter(width = 1) +
-  geom_line(data = pred.piece, aes(y = lbound), color = "black") +
-  geom_line(data = pred.piece, aes(y = ubound), color = "red") +
-  facet_wrap(~ region) +
-  labs(caption = "y = 1/n (black) and 2(p+1)/n (red)", title = "Leverages") +
+ggplot() +
+   #geom_point(data = pred.piece.linear, aes(x = fit, y = e)) +
+   geom_point(data = pred.piece.poly, aes(x = fit, y = e)) +
+  #geom_point(data = pred.piece.log, aes(x = fit, y = e)) +
+  facet_wrap(~ region, scales = "free") +
+  #labs(caption = "y = 1/n (black) and 2(p+1)/n (red)", title = "Leverages") +
   theme(text = element_text(size = 18)) +
-  geom_hline(yintercept = 0, color = NA)
+  geom_hline(yintercept = 0, color = "transparent")
+
+ggplot(data = pred.piece.log, aes(sample = e)) +
+  geom_qq() +
+  geom_qq_line() +
+  #facet_wrap(~ region, scales = "free") +
+  #labs(caption = "y = 1/n (black) and 2(p+1)/n (red)", title = "Leverages") +
+  theme(text = element_text(size = 18))
+
+ggplot() +
+  geom_point(data = pred.piece.linear, aes(x = fit, y = r, color = 'linear')) +
+  geom_point(data = pred.piece.log, aes(x = fit, y = r, color = 'log')) +
+  geom_point(data = pred.piece.poly, aes(x = fit, y = r, color = "poly")) +
+  geom_hline(yintercept = 0) +
+  geom_hline(yintercept = c(-2, 2), color = "red") +
+  geom_hline(yintercept = c(-4, 4), color = "red", linetype = "dashed") +
+  facet_wrap( ~region, scales = 'free_x') +
+  xlab("Fitted values") +
+  ylab("r*") +
+  labs(title = "Pike: studentized residuals vs fitted values") +
+  labs(caption = "y = +/- 2 and +/- 4") +
+  theme(text = element_text(size = 18))
+
 
 
 model.piece.reduced <- step(model.piece, k = log(nrow(chosen.data)))
@@ -153,3 +195,18 @@ ggplot(fhm.data[fhm.data$region == reg,], aes(x = day_nbr_region, y = new_cases)
             color = "red", linetype = "dashed", size = 1) +
   geom_line(data = pred.piece.longer[pred.piece.longer$region == reg,], aes(y = conf.upr),
             color = "red", linetype = "dashed", size = 1)
+
+
+
+reduced <- c('Västmanland', 'Dalarna', 'Örebro')
+
+pred.piece.linear.red <- pred.piece.linear[pred.piece.linear$region %in% reduced,]
+
+ggplot() +
+  geom_point(data = pred.piece.linear.red, aes(x = fit, y = e, color = region)) +
+  #geom_point(data = pred.piece.poly, aes(x = fit, y = e)) +
+  #geom_point(data = pred.piece.log, aes(x = fit, y = e)) +
+  #facet_wrap(~ region, scales = "free") +
+  #labs(caption = "y = 1/n (black) and 2(p+1)/n (red)", title = "Leverages") +
+  theme(text = element_text(size = 18)) +
+  geom_hline(yintercept = 0, color = "transparent")
